bcaf9b9fd95f83005ca5d9659596c00c9b7a88cf
Ankur Patel <135263430+Ankur-Patel-111@users.noreply.github.com>
2023-12-27 17:07:36 +0530

    Add files via upload

---
+++ Python Vector/demo.py
@@ -0,0 +1,162 @@
+#print("Jay shree krishna")
+#print("hello world")
+#print("Hello Python")
+# this is a comment.
+""" more than one line comment  """
+
+# (variable) in python variable are containers for stroing data values.
+"""
+x=10
+y="Ankur"
+print(x)
+print(y)
+"""
+# same variable name decalire
+x=10
+#x="Bangalore" 
+#print(x)
+
+# casting (data-type)
+x=str(5)
+y=int(10)
+z=float(15)
+#print(x,y,z)
+
+#get the type () function. 
+# x=20
+y="Ankur"
+#print(type(x))
+#print(type(y))
+
+#variable name in python 
+my_var = "john"
+_my_var = "john"
+my_Var = "john"
+
+#print(my_var)
+#print(_my_var)
+
+"""
+#python allows to multiple variable in one line.
+x,y,z=("orange","banana","mango")
+print(x,y)
+print(y)
+print(z)
+"""
+
+#one variable to multiple variable
+x=y=z="Apple"
+#print(x)
+#print(y)
+
+#print("Today is a good day . Today is a sunday. 20-08-2023 ")
+
+#if 10>5:
+ #   print("ten is greater than five")
+
+# range in python
+"""
+x=range(6)
+print(x)
+print(type(x))    
+"""
+#python in numbers int,float,complex.
+x=10   #integer
+y=20.5 #float
+z=3+5j #complex
+"""
+print(type(x))
+print(type(y))
+print(type(z))
+"""
+"""
+#type conversion one type to another type
+x=5  #int
+y=10.5 #float
+z=1+2j #complex
+
+a=float(x)
+b=int(y)
+c=complex(z)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
+"""
+"""
+#typecasting in python.
+a="5"
+print(a)
+b=5
+print(b+5)
+"""
+ #-------------------------------------------OPERATOR IN PYTHON-----------------------------------
+"""
+#Arithmatic operator
+num1=10
+num2=5
+print("num1 + num2 is:",num1 + num2)
+print("num1 - num2 is:",num1 - num2)
+print("num1 * num2 is:",num1 * num2)
+print("num1 / num2 is:",num1 / num2)
+"""
+"""
+# Assignment operator . = is using for Assigning the value.
+a=5
+print(a)
+a+=5
+print(a)
+"""
+
+#comparision operator
+a=5
+b=10
+c=5
+#print(a<b)
+#print(a>b)
+#print(a==c) # here == is using in comparision operator, only = is use for Assinging the value.
+
+#Logical operator
+#print(a==c and a>b )
+#print(a==c or a<=c )
+"""
+# python in string 
+name = "ankur"
+print(name)
+print(name[0:3]) # all most programming language number start to 0.= 2 (n-1) (3-1=2)
+print(name.upper())
+#print(name.capitalize()) #in a string all first small character is convert to capitalize.
+#print(name.count("k"))
+#print(name.isnumric())
+"""
+
+"""
+# in python how to take user input minute 47:00.
+
+name = input("Enter the name: ")
+print(name) 
+"""
+
+"""
+# write a python program to take user input number 6
+
+num = input("Enter the number: ")
+print(type(num))
+print(int (num) + 6)
+
+#----------- last 51:11 second -------------list is next topic.
+"""
+"""
+name = input("Enter the name: ")
+print(name)
+num = input("Enter the nuber: ")
+print(num)
+"""
+word = "live"
+print(len(word))
+
+
